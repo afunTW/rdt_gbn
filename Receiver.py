d18577256df635670206eff521ee4ee61806e0dd
endlessproof <endlessproof@gmail.com>
2015-03-29 04:50:25 -0230

    UPDATE README

--- Receiver.py
+++ Receiver.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 
 import sys
+
+from rdt import *
 from tcpSocket import *
 
 if __name__ == '__main__':
 	DEST = 'localhost';
 	DESTPORT = 8888;
+	SRCPORT = 9999;
 
-	receiver = tcpSocket(9999);
-	receiver.connect(DEST, DESTPORT);
-	receiver.listen();
-	receiver.recv();
-	receiver.close();
+	dt = 
+
+	# receiver = tcpSocket(9999);
+	# receiver.connect(DEST, DESTPORT);
+	# receiver.listen();
+	# receiver.recv();
+	# receiver.close();
