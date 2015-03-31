232afc5314590fee0cc36bdc84028be8773e0197
endlessproof <endlessproof@gmail.com>
2015-03-31 21:24:11 -0230

    ADD get function to access each field in the form of packet

--- checksum.py
+++ checksum.py
@@ -14,8 +14,15 @@ def generate_checksum(data):
 
 	return format(int(check,2)^0xffff, '016b');	# 1's complement
 
-def corrupt(pkt):
-	checksum = pkt[96:112];
+def valid_ckecksum(pkt):
+	checksum = getchecksum(pkt);
 	data = pkt[0:96]+pkt[112:];
-
 	return True if generate_checksum(data) == checksum else False;
+
+def getsrcport(pkt): return pkt[0:16];
+def getdestport(pkt): return pkt[16:32];
+def getseq(pkt): return pkt[32:64];
+def getack(pkt): return pkt[64:96];
+def getchecksum(pkt): return pkt[96:112];
+def getpayload(pkt): return pkt[112:];
+def getheader(pkt): return pkt[0:112];
