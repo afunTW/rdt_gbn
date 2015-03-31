4654a24ed9f1da94bd3a14d4a6d629d83003b240
endlessproof <endlessproof@gmail.com>
2015-03-31 21:02:12 -0230

    NEW timer.py

---
+++ timer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import sys
+import time
+
+class timer():
+	def __init__(self):
+		if sys.platform == 'win32':
+			self.default_timer = time.clock;	# On Windows, the best timer is time.clock
+		else:
+			self.default_timer = time.time;	# On most other platforms the best timer is time.time
+	def settime(self, sec):
+		self.set_time = sec;
+		print("Set timer: %d sec" % self.set_time);
+	def start(self):
+		self.start_time = self.default_timer();
+		print("Start timer: ", self.start_time);
+	def timeout(self):
+		return True if self.default_timer()-self.start_time > self.set_time else False;
